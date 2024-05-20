b812422d313c21f8fac7615abd5f38ca9c8f32cd
Nanthagopal Shanmugam <prutuja189@gmail.com>
2024-05-20 15:40:36 +0530

    Structure_changes_added

--- Home/Home/Controllers/v1/ManageUsersController.cs
+++ Home/Home/Controllers/v1/ManageUsersController.cs
@@ -4,11 +4,19 @@ using System.Net;
 using Utilities.Helpers;
 
 namespace Home.API.Controllers.v1;
-[Route("ap/[controller]")]
+[Route("api/[controller]")]
 [ApiController]
 public class ManageUsersController : ControllerBase
 {
+    /// <summary>
+    /// API For user login
+    /// </summary>
+    /// <param name="loginValidation"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
     [HttpPost("Login")]
+    [ProducesResponseType(typeof(LoginValidationModel), (int)HttpStatusCode.OK)]
+    [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.BadRequest)]
     public ActionResult<LoginValidationModel> Login(LoginValidationModel loginValidation, CancellationToken cancellationToken)
     {
         var result = loginValidation;
